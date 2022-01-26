3b3da138320f02d3507915975cb5d472494612fb
Lee <553269487@qq.com>
2022-01-25 20:54:17 -0800

    Time: 1 ms (46.82%), Space: 39.9 MB (20.33%) - LeetHub

--- 21-merge-two-sorted-listsa-idsolution-btn-21-hrefhttps-labuladonggiteeio-plugin-v3-qno21-and-amp-targetgitee-and-amp-1643171207373-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-21-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/21-merge-two-sorted-listsa-idsolution-btn-21-hrefhttps-labuladonggiteeio-plugin-v3-qno21-and-amp-targetgitee-and-amp-1643171207373-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-21-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 21-merge-two-sorted-listsa-idsolution-btn-21-hrefhttps-labuladonggiteeio-plugin-v3-qno21-and-amp-targetgitee-and-amp-1643171207373-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-21-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/21-merge-two-sorted-listsa-idsolution-btn-21-hrefhttps-labuladonggiteeio-plugin-v3-qno21-and-amp-targetgitee-and-amp-1643171207373-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-21-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -10,29 +10,34 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode cur = new ListNode();
-        ListNode dummy = cur;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode head, cur;
+        if(l1.val < l2.val){
+            head = l1;
+            l1 = l1.next;
+        }else{
+            head = l2;
+            l2 = l2.next;
+        }
         
-        while(true){
-            if(l1 == null){
-                dummy.next = l2;
-                break;
-            }
-            else if(l2 == null){
-                dummy.next = l1;
-                break;
-            }
-            
-            else if(l1.val < l2.val){
-                dummy.next = l1;
+        cur = head;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                cur.next = l1;
                 l1 = l1.next;
             }else{
-                dummy.next = l2;
+                cur.next = l2;
                 l2 = l2.next;
             }
-            
-            dummy = dummy.next;
+            cur = cur.next;
         }
-        return  cur.next;
+        
+        cur.next = l1 == null? l2 : l1;
+        return head;
+        
+        
+        
     }
 }
