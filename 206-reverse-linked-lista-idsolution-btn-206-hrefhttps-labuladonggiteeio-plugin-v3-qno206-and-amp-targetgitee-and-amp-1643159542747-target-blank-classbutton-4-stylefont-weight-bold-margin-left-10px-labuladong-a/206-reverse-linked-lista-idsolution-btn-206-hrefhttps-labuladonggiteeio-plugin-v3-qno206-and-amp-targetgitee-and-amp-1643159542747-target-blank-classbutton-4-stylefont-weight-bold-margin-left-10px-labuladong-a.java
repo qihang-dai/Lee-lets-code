dba2ff1ff67df777bd39f29c2d215731bc409a57
Lee <553269487@qq.com>
2022-01-25 18:12:12 -0800

    Time: 0 ms (100.00%), Space: 39.9 MB (28.63%) - LeetHub

--- 206-reverse-linked-lista-idsolution-btn-206-hrefhttps-labuladonggiteeio-plugin-v3-qno206-and-amp-targetgitee-and-amp-1643159542747-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-a/206-reverse-linked-lista-idsolution-btn-206-hrefhttps-labuladonggiteeio-plugin-v3-qno206-and-amp-targetgitee-and-amp-1643159542747-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-a.java
+++ 206-reverse-linked-lista-idsolution-btn-206-hrefhttps-labuladonggiteeio-plugin-v3-qno206-and-amp-targetgitee-and-amp-1643159542747-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-a/206-reverse-linked-lista-idsolution-btn-206-hrefhttps-labuladonggiteeio-plugin-v3-qno206-and-amp-targetgitee-and-amp-1643159542747-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-a.java
@@ -10,14 +10,13 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        //head .next is enough. head == null is simply for empty list
-        if(head == null || head.next == null) return head;
-        //in this recursion, push to the last Node, and do reverse from the end.
-        // to
-        ListNode last = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return last;
-        
+        ListNode pre = null, cur = head, tmp;
+        while(cur != null){
+            tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
     }
 }
