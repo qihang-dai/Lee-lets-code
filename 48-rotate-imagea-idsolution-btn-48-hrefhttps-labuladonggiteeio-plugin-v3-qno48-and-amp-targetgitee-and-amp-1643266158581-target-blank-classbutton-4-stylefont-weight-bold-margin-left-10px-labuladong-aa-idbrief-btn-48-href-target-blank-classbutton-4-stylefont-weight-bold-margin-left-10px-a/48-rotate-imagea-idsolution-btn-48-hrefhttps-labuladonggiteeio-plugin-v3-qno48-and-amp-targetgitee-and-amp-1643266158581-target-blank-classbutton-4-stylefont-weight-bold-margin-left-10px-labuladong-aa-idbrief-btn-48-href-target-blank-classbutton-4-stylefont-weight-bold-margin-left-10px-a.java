5e812685bee81044a896bbf6c98f5c1a6c512ee0
Lee <553269487@qq.com>
2022-01-26 23:02:52 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (6.81%) - LeetHub

--- 48-rotate-imagea-idsolution-btn-48-hrefhttps-labuladonggiteeio-plugin-v3-qno48-and-amp-targetgitee-and-amp-1643266158581-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-48-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/48-rotate-imagea-idsolution-btn-48-hrefhttps-labuladonggiteeio-plugin-v3-qno48-and-amp-targetgitee-and-amp-1643266158581-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-48-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 48-rotate-imagea-idsolution-btn-48-hrefhttps-labuladonggiteeio-plugin-v3-qno48-and-amp-targetgitee-and-amp-1643266158581-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-48-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/48-rotate-imagea-idsolution-btn-48-hrefhttps-labuladonggiteeio-plugin-v3-qno48-and-amp-targetgitee-and-amp-1643266158581-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-48-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -3,17 +3,17 @@ class Solution {
         int n = matrix.length;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++){ //镜像反转， 需要j < i 沿着对角线
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
             }
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n/2; j++){
+        for(int i = 0; i < n; i++){ 
+            for(int j = 0; j < n/2; j++){ //对称反转， 奇数偶数都是小于2/n
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][j] = matrix[i][n - j - 1]; // 也可以看做是 (n - 1) - j， 二者相加index为n-1，确实对称
                 matrix[i][n - j - 1] = tmp;
             }
         }
