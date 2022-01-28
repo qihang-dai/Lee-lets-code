dceabbdadfb2ba61b39289561050c56fd7ffbe4a
Lee <553269487@qq.com>
2022-01-27 16:33:49 -0800

    Time: 4 ms (24.21%), Space: 47.5 MB (5.88%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,18 +1,29 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        for(int[] cur : intervals){
-            if(cur[1] < newInterval[0]){
-                res.add(cur);
-            }else if (cur[0] > newInterval[1]){
-                res.add(newInterval);
-                newInterval = cur; // 防止重复添加的办法就是占位
-            }else{
-                newInterval[0] = Math.min(cur[0], newInterval[0]);
-                newInterval[1] = Math.max(cur[1], newInterval[1]);
-            }
+        List<int[]> res = new ArrayList();
+        int i = 0;
+        
+        while(i < intervals.length && intervals[i][1] < newInterval[0]){
+            res.add(intervals[i]);
+            i++;
+        }
+        
+        while(i < intervals.length && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
         }
         res.add(newInterval);
+        
+        while(i < intervals.length){
+            res.add(intervals[i++]);
+        }
+        
         return res.toArray(new int[0][0]);
+        
+        
+        
+        
+        
     }
 }
