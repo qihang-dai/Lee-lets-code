1fe01d0d1bbc7ed2d3723b9979a8cb677112c9d8
Lee <553269487@qq.com>
2022-01-30 16:20:27 -0800

    Time: 17 ms (85.82%), Space: 48.2 MB (55.28%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.java
+++ 436-find-right-interval/436-find-right-interval.java
@@ -1,35 +1,17 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        Map<int[], Integer> map = new HashMap();
+        TreeMap<Integer, Integer> map = new TreeMap();
         int n = intervals.length;
         int[] res = new int[n];
         for(int i = 0; i < n; i++){
-            map.put(intervals[i], i);
+            map.put(intervals[i][0], i);
         }
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         for(int i = 0; i < n; i++){
-            int[] right = binarySearch(intervals, intervals[i][1], 0, intervals.length - 1);
-            res[map.get(intervals[i])] = right == null? -1 : map.get(right);
+            Map.Entry<Integer, Integer> pos = map.ceilingEntry(intervals[i][1]);
+            res[i] = pos == null ? -1 : pos.getValue();
         }
         return res;
     }
     
-    int[] binarySearch(int[][] intervals, int right, int low, int high){
-        if(low >= high){
-            if(intervals[low][0] >= right){
-                return intervals[low];
-            }
-            return null;
-        }
-        
-        int mid = low + (high - low)/2;
-
-        if(intervals[mid][0] < right){
-            return binarySearch(intervals, right, mid + 1, high);
-        }else{
-            return binarySearch(intervals, right, low, mid);
-        }
-        
-    }
         
 }
