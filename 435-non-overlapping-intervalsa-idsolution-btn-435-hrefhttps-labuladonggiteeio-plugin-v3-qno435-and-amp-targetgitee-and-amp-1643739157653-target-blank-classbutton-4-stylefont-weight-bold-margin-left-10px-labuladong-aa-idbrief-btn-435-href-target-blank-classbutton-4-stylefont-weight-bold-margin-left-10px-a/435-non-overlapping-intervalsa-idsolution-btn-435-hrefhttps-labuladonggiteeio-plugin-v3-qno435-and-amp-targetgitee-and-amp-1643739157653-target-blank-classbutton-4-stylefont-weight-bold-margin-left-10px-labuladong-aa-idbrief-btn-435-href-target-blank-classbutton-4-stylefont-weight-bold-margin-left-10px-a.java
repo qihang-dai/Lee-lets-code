0cef346a34be839e936e4826bc3214c8a00c79ca
Lee <553269487@qq.com>
2022-02-01 11:33:18 -0800

    Time: 120 ms (16.89%), Space: 97.7 MB (57.33%) - LeetHub

--- 435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1643739157653-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1643739157653-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1643739157653-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/435-non-overlapping-intervalsa-idsolution-btn-435-hrefhttps-labuladonggiteeio-plugin-v3-qno435-and-amp-targetgitee-and-amp-1643739157653-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-435-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -2,14 +2,17 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> a[1] - b[1]);
         int count = 1;
+        int res = -1;
         int xend = intervals[0][1];
         for(int[] intvs : intervals){
             if(intvs[0] >= xend){
                 count++;
                 xend = intvs[1];
+            }else{
+                res++;
             }
         }
-        return intervals.length - count;
+        return res;
         
     }
 }
