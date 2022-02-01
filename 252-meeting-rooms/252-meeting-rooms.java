ec59dbb76862992040b9e6d12f0befa57044bbb5
Lee <553269487@qq.com>
2022-02-01 12:53:15 -0800

    Time: 9 ms (48.30%), Space: 45.6 MB (5.37%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,30 +1,12 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        if(intervals.length == 0) return true;
-        int n = intervals.length;
-        int[] start = new int[n];
-        int[] end = new int[n];
-        for(int i = 0; i < n; i++){
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
-        }
-        Arrays.sort(start);
-        Arrays.sort(end);
-        int count = 0;
-        int res = 0;
-        int i = 0, j = 0;
-        while(i < n && j < n){//i肯定先到达n，至于最后j结束与否也无所谓了，就是求最多重叠次数。
-            if(start[i] < end[j]){
-                count++;
-                i++;
-            }else{
-                count--;
-                j++;
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        for(int i = 0; i < intervals.length - 1; i++){
+            if(intervals[i][1] > intervals[i + 1][0]){
+                return false;
             }
-            res = Math.max(res, count);
         }
-        // System.out.println(res);
-        return res - 1 == 0;
+        return true;
         
     }
 }
