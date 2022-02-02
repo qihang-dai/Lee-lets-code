1d8bb8c286a7068372316567c44af3c5d931002b
Lee <553269487@qq.com>
2022-02-01 23:10:10 -0800

    Time: 1 ms (100.00%), Space: 43.2 MB (57.46%) - LeetHub

---
+++ 285-inorder-successor-in-bsta-idbrief-btn-285-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/285-inorder-successor-in-bsta-idbrief-btn-285-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null) return null;
+        TreeNode successor = null;
+        if(root.val > p.val){
+            successor = inorderSuccessor(root.left, p);
+            if(successor == null){
+                successor = root;
+            }
+        }
+        if(root.val < p.val){
+           successor = inorderSuccessor(root.right, p);
+        }
+        if(root.val == p.val){
+            successor = getMin(root.right);
+        }
+        return successor;
+    }
+    
+    TreeNode getMin(TreeNode p){
+        while(p != null && p.left != null){
+            p = p.left;
+        }
+        return p;
+    }
+}
