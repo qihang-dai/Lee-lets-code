185b9e43f4c3f93e903bf9cc70ee356f5f54bf7c
Lee <553269487@qq.com>
2022-02-02 21:41:44 -0800

    Time: 4 ms (49.09%), Space: 67.5 MB (8.47%) - LeetHub

---
+++ 55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;
+        int cover = nums[0];
+        for(int i = 1; i <= cover; i++){
+            cover = Math.max(i +nums[i], cover);
+            if(cover >= nums.length - 1) return true;
+        }
+        return false;
+        
+    }
+}
