016c7922d5776b27bf1afb9ca8ed366a48181220
Lee <553269487@qq.com>
2022-02-02 21:42:07 -0800

    Time: 3 ms (62.93%), Space: 67.5 MB (8.12%) - LeetHub

--- 55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
+++ 55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a/55-jump-gamea-idsolution-btn-55-hrefhttps-labuladonggiteeio-plugin-v3-qno55-and-amp-targetgitee-and-amp-1643848189178-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-labuladong-aa-idbrief-btn-55-href-target-blank-classbutton-4-stylefont-weight-bold-margin-left-10px-a.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length <= 1) return true;
+        if(nums.length == 1) return true;
         int cover = nums[0];
         for(int i = 1; i <= cover; i++){
             cover = Math.max(i +nums[i], cover);
