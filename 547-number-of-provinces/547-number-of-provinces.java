506837e1f23380c96e9b8f8cee4301f9d465d7ac
Lee <553269487@qq.com>
2022-02-07 14:59:49 -0800

    Time: 3 ms (45.74%), Space: 43.1 MB (60.91%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,68 +1,63 @@
-class UnionFind {
-    // 记录父节点
-    private Map<Integer,Integer> father;
-    // 记录集合的数量
-    private int numOfSets = 0;
+class UF{
+    int size;
+    Map<Integer, Integer> father;
     
-    public UnionFind() {
+    UF(){
         father = new HashMap<Integer,Integer>();
-        numOfSets = 0;
+        size = 0;
     }
     
-    public void add(int x) {
-        if (!father.containsKey(x)) {
+    void add(int x){
+        if(!father.containsKey(x)){
             father.put(x, null);
-            numOfSets++;
+            size++;
         }
     }
     
-    public void merge(int x, int y) {
+    void merge(int x, int y){
         int rootX = find(x);
         int rootY = find(y);
-        
-        if (rootX != rootY){
-            father.put(rootX,rootY);
-            numOfSets--;
+        if(rootX != rootY){
+            father.put(rootX, rootY);
+            size--;
         }
     }
     
-    public int find(int x) {
+    int find(int x){
         int root = x;
-        
         while(father.get(root) != null){
             root = father.get(root);
         }
-        
         while(x != root){
-            int original_father = father.get(x);
-            father.put(x,root);
-            x = original_father;
+            int of = father.get(x);
+            father.put(x, root);
+            x = of;
         }
-        
         return root;
     }
     
-    public boolean isConnected(int x, int y) {
+    boolean isConnected(int x, int y){
         return find(x) == find(y);
     }
     
-    public int getNumOfSets() {
-        return numOfSets;
+    int size(){
+        return size;
     }
+    
 }
 
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        UnionFind uf = new UnionFind();
-        for(int i = 0;i < isConnected.length;i++){
+        UF uf = new UF();
+        for(int i = 0; i < isConnected.length; i++){
             uf.add(i);
-            for(int j = 0;j < i;j++){
+            for(int j = 0; j < i; j++){
                 if(isConnected[i][j] == 1){
-                    uf.merge(i,j);
+                    uf.merge(i, j);
                 }
             }
         }
+        return uf.size();
         
-        return uf.getNumOfSets();
     }
 }
