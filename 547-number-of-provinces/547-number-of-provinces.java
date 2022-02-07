46624c2804fc6d9d168d0e8135b5efa4e43847b8
Lee <553269487@qq.com>
2022-02-07 00:45:03 -0800

    Time: 5 ms (27.15%), Space: 54.5 MB (5.74%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,23 +1,68 @@
+class UnionFind {
+    // 记录父节点
+    private Map<Integer,Integer> father;
+    // 记录集合的数量
+    private int numOfSets = 0;
+    
+    public UnionFind() {
+        father = new HashMap<Integer,Integer>();
+        numOfSets = 0;
+    }
+    
+    public void add(int x) {
+        if (!father.containsKey(x)) {
+            father.put(x, null);
+            numOfSets++;
+        }
+    }
+    
+    public void merge(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if (rootX != rootY){
+            father.put(rootX,rootY);
+            numOfSets--;
+        }
+    }
+    
+    public int find(int x) {
+        int root = x;
+        
+        while(father.get(root) != null){
+            root = father.get(root);
+        }
+        
+        while(x != root){
+            int original_father = father.get(x);
+            father.put(x,root);
+            x = original_father;
+        }
+        
+        return root;
+    }
+    
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+    
+    public int getNumOfSets() {
+        return numOfSets;
+    }
+}
+
 class Solution {
-    public int findCircleNum(int[][] M) {
-        int[] visited = new int[M.length];
-        int count = 0;
-        Queue<Integer> q = new LinkedList();
-        for(int i = 0; i < M.length; i++){
-            if(visited[i] == 0){
-                q.add(i);
-                while(!q.isEmpty()){
-                    int node = q.poll();
-                    visited[node] = 1;
-                    for(int j = 0; j < M.length; j++){
-                        if(M[node][j] == 1 && visited[j] == 0)
-                            q.add(j);
-                    } 
+    public int findCircleNum(int[][] isConnected) {
+        UnionFind uf = new UnionFind();
+        for(int i = 0;i < isConnected.length;i++){
+            uf.add(i);
+            for(int j = 0;j < i;j++){
+                if(isConnected[i][j] == 1){
+                    uf.merge(i,j);
                 }
-                count++;
             }
         }
-        return count;
         
+        return uf.getNumOfSets();
     }
 }
