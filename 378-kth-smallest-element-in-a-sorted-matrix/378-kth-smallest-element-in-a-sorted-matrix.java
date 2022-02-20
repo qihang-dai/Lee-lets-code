b616627e4825be0cda7d55ca326efeb336d00cc4
Lee <553269487@qq.com>
2022-02-19 23:21:12 -0500

    Time: 1 ms (88.92%), Space: 57.1 MB (16.53%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,25 +1,18 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        PriorityQueue<Tuple> q = new PriorityQueue<Tuple>((a,b) -> a.val - b.val);
-        for(int i = 0; i < n; i++){
-            q.offer(new Tuple(0, i, matrix[0][i]));
+        int low = matrix[0][0], high = matrix[n - 1][n - 1];
+        while(low < high){
+            int mid = low + (high - low)/2;
+            int count = 0, j = n - 1;
+            for(int i = 0; i < n; i++){
+                while(j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
+            if(count < k) low = mid + 1;
+            else high = mid;
         }
+        return low;
         
-        for(int i = 0; i < k -1; i++){
-            Tuple t = q.poll();
-            if(t.x == n - 1) continue;
-            q.offer(new Tuple(t.x + 1, t.y, matrix[t.x + 1][t.y]));
-        }
-        return q.poll().val;
-    }
-}
-
-class Tuple{
-    int x, y, val;
-    public Tuple(int x, int y, int val){
-        this.x = x;
-        this.y = y;
-        this.val = val;
     }
 }
