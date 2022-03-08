2ed67621f6ff7abd20cc2b26084a3da2a5292a4b
Lee <553269487@qq.com>
2022-03-07 17:26:41 -0800

    Time: 4 ms (28.29%), Space: 44.8 MB (23.95%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -7,12 +7,12 @@ class Solution {
     
     public void dfs(List<List<Integer>> res, List<Integer> path, int[] nums){
         if(path.size() == nums.length){
-            res.add(new ArrayList(path));
+            res.add(new ArrayList(path)); 
             return;
         }
         
         for(int i = 0; i < nums.length; i++){
-            if(path.contains(nums[i])) continue;
+            if(path.contains(nums[i])) continue; //
             path.add(nums[i]);
             dfs(res, path, nums);
             path.remove(path.size() - 1);
