88d5a21218791ac4f0fc3df85e293bf83d3b8457
Lee <553269487@qq.com>
2022-03-09 20:17:51 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (6.65%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,31 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);   
-    }
-    
-    public void quickSort(int[] nums, int start, int end){
-        if(start < end){
-            int position = partition(nums, start, end);
-            quickSort(nums, start, position - 1);
-            quickSort(nums, position +1, end);
+        int[] count = new int[3];
+        for(int n : nums){
+            count[n]++;
         }
-    }
 
-    
-    public int partition(int[] nums, int start, int end){
-        int p = start;
-        for(int i = start; i < end; i++){
-            if(nums[i] <= nums[end]){
-                int tmp = nums[i];
-                nums[i] = nums[p];
-                nums[p] = tmp;
-                p++;
+        int idx = 0;
+        for(int i = 0; i < 3; i++){
+            while(count[i] > 0){
+                nums[idx] = i;
+                count[i]--;
+                idx++;
             }
         }
-        
-        int tmp = nums[p];
-        nums[p] = nums[end];
-        nums[end] = tmp;
-        return p;
+
     }
 }
