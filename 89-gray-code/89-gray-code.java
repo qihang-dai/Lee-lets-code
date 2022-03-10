f4d1aae70a54ddc49e4b35e934a7c915539f36fe
Lee <553269487@qq.com>
2022-03-09 23:05:05 -0800

    Time: 8 ms (77.16%), Space: 55.7 MB (38.38%) - LeetHub

---
+++ 89-gray-code/89-gray-code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList(){{add(0);}};
+        int head = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = res.size() - 1; j >= 0; j--){
+                res.add(head + res.get(j));
+            }
+            head = head << 1;
+        }
+        return res;
+    }
+}
