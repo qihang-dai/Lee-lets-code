7cb936ed802fff79d48ca5f0782e7d0a1b1a3dea
Lee <553269487@qq.com>
2022-03-30 17:42:09 -0700

    Time: 1 ms (92.31%), Space: 41.8 MB (99.75%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.java
+++ 1629-slowest-key/1629-slowest-key.java
@@ -3,16 +3,18 @@ class Solution {
         int n = releaseTimes.length;
         int[] pre = new int[n];
         int max = releaseTimes[0];
-        for(int i = 1; i < n; i++){
-            pre[i] = releaseTimes[i] - releaseTimes[i - 1];
-            max = Math.max(pre[i], max);
-        }
         pre[0] = releaseTimes[0];
-        
-        char res = 'a';
-        for(int i = 0; i < n; i++){
+        char res = keysPressed.charAt(0);
+        for(int i = 1; i < n; i++){
+            int diff = releaseTimes[i] - releaseTimes[i - 1];
             char c = keysPressed.charAt(i);
-            if(pre[i] == max && c > res){
+
+            if(diff > max){
+                max = diff;
+                res = c;
+            }
+            
+            if(diff == max && c > res){
                 res = c;
             }
         }
