8cc90e409f844867bb19266b524b69b4e4e0df6b
Lee <553269487@qq.com>
2022-04-21 10:22:06 -0700

    Time: 2 ms (97.99%), Space: 42 MB (99.55%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,34 +1,41 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int i = 0, j = 0, istar = -1, jstar = -1;
-        int m = s.length(), n = p.length();
-        // some special case:
-        // if(m < n) return false; this is wrong, cause we can have endless star ***
+        int i = 0, j = 0, m = s.length(), n = p.length();
+        
+        int istar = -1, jstar = -1;
         while(i < m){
-            //condition 1: single char match                         //if after star we match something (star can be 0 or 1), we ++
-            if(j < n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')){
+            
+            //condition 1: exact match
+            if(j < n && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))){
                 i++;
                 j++;
-            //condtiion 2: match star： if there is one start, it can match as many as possible. If there is two consecutive star, the first one is not useful
-            }else if(j < n && p.charAt(j) == '*'){
-                //we record the matched sINdex, and the next real char j index, then another loop
-                istar = i;
-                jstar = j++;
-            }else if(istar != -1){ // if we have a star, but we match it wrong, we get j and i back to the star position, we greedily need match star in i once
-                i = ++istar;        // the greedy would work if we always need to match it, then the loop would run.
-                j = jstar + 1;
-            }else{
+            }
+            
+            //condition 2: we record every last star we have. So we can return to the last star if we mismatch
+            else if(j < n && p.charAt(j) == '*'){
+                istar = i; //matched position, but i not move, cause we can match empty
+                jstar = j++; // star position, but j move cause the star is in use
+            }
+            
+            //condition 3: no match, but we have star, we can greedily rerun the match from the last star position
+            // this time we have to match one more 
+            else if(istar != -1){
+                i = ++istar; // istar moved, cause we defo need to match one more
+                j = jstar + 1; // j return to the last star
+                
+            }
+            
+            //condition 4: no star, no match, or reach the length limited of j while i is not matched enough
+            else{
                 return false;
             }
         }
         
-        //at the end, i is matched. Lets see if j has only left star *
+        //now we have matched all the s, but dunno p pattern string is matched all.
+        while(j < n && p.charAt(j) == '*') j++;
         
-        while(j < n){
-            if(p.charAt(j) != '*') return false;
-            j++;
-        }
-        return true;
+        //if all j left is *, its a match, else not
+        return j == n;
         
     }
 }
