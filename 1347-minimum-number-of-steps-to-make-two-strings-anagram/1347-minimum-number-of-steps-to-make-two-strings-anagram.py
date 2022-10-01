e512bc16459c8a16716487a19d916a290e05b856
Qihang Dai <qihangdai98@gmail.com>
2022-09-30 20:46:46 -0700

    Time: 454 ms (35.90%), Space: 14.5 MB (77.67%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        cnt, steps = Counter(s), 0
+        
+        for c in t:
+            if cnt[c]:
+                cnt[c] -= 1
+            else:
+                steps += 1
+        
+        return steps
+        
