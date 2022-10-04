cc232f9da3be50715986cb4dcf3a93319be9bc23
Qihang Dai <qihangdai98@gmail.com>
2022-10-03 22:39:08 -0700

    Time: 314 ms (92.54%), Space: 16.5 MB (48.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
          
         def dfs(i, j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1': 
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == '0': 
                 return
             grid[i][j] = '0'
             dfs(i + 1, j)
