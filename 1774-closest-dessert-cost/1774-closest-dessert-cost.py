d89c1060e1107217c22c8e30277fd30eb26e1009
Qihang Dai <qihangdai98@gmail.com>
2022-10-14 23:03:35 -0700

    Time: 931 ms (44.60%), Space: 26.8 MB (12.92%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,12 +1,25 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:        
-        q = [target - basecost for basecost in baseCosts]
-
-        for toppingcost in toppingCosts:
-            q += [val - cost for val in q if val > 0 for cost in (toppingcost, toppingcost * 2)]
+        costs = set(baseCosts)
+        for topping in toppingCosts:
+            withToppingCosts = []
+            for c in costs:
+                withToppingCosts.append(c)
+                withToppingCosts.append(c+topping)
+                withToppingCosts.append(c+topping*2)
+            print(withToppingCosts)
+            costs = set(withToppingCosts)
+        print(costs)
         
-        print(q)
-        return target - min(q, key=lambda x: 2 * abs(x) - (x > 0))
+        # Set now contains all the possibilitys. Pick the best one.
+        best = baseCosts[0]
+        for c in costs:
+            if abs(c-target) < abs(best-target):
+                best = c
+            elif abs(c-target) == abs(best-target) and c < best:
+                best = c
+        return best
+
         
             
         
