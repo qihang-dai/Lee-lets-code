dcfc87229e5c0279a12b02028bab68083ac33c9a
Qihang Dai <qihangdai98@gmail.com>
2022-10-15 17:07:43 -0700

    Time: 50 ms (77.96%), Space: 13.9 MB (82.82%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+
+        
+        res = []
+        path = []
+        
+        if len(s) > 12:
+            return res
+        
+        def backtrack(path, start):
+            if len(path) == 4:
+                if start == len(s):
+                    pathString = ".".join(path)
+                    res.append(pathString)
+                    return
+            for i in range(start + 1, min(len(s) + 1, start + 4)):
+                tmp = s[start: i]
+                if len(tmp) > 1 and int(tmp[0]) == 0:
+                    break
+                if len(tmp) > 2 and int(tmp) > 255:
+                    break
+                
+                path.append(tmp)
+                backtrack(path, i)
+                path.pop()
+        
+        backtrack([], 0)
+        return res
+            
+                    
+                
+                
