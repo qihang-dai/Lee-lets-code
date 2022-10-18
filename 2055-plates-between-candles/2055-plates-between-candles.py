7f19ee675607a0706aebe1fd54f7501857c93242
Qihang Dai <qihangdai98@gmail.com>
2022-10-18 13:15:49 -0700

    Time: 4884 ms (32.15%), Space: 53.8 MB (19.64%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,12 +1,10 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         A = [i for i, c in enumerate(s) if c == '|']
-        print(A)
         res = []
         for a, b in queries:
             i = bisect_left(A, a)
             j = bisect_right(A, b) - 1
-            print(a, b, i, j)
             res.append(A[j] - A[i] + 1 - (j - i + 1) if i < j else 0)
         return res
         
